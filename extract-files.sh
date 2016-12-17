21abc18373411be0cad5686efa1f7a45e115c7aa
martincz <martincz@mokeedev.com>
2016-12-17 14:01:24 +0800

    wt88047: mokee bringup

--- extract-files.sh
+++ extract-files.sh
@@ -24,9 +24,9 @@ VENDOR=wingtech
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+MK_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+HELPER="$MK_ROOT"/vendor/mk/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -52,7 +52,7 @@ fi
 "$CM_ROOT"/device/qcom/common/extractors/extract-files.sh "$SRC" msm8916 graphics
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$MK_ROOT"
 
 extract "$MY_DIR"/proprietary-files-qc.txt "$SRC"
 extract "$MY_DIR"/proprietary-files.txt "$SRC"
