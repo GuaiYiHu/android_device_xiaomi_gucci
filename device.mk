d59795803ac1c2d88954eb1ebee66275417d5dcc
guaiyihu <guaiyihu@foxmail.com>
2018-07-18 01:27:23 +0800

    gucci: rootdir: Import init.qcom.post_boot.sh from stock
    Change-Id: I8bb675d4fd12295e92af959cced4928dd6ee6f46

--- device.mk
+++ device.mk
@@ -204,7 +204,8 @@ PRODUCT_PACKAGES += \
     init.crda.sh \
     init.qcom.bt.sh \
     init.qcom.coex.sh \
-    init.qcom.fm.sh
+    init.qcom.fm.sh \
+    init.qcom.post_boot.sh
 
 PRODUCT_PACKAGES += \
     init.class_main.sh \
