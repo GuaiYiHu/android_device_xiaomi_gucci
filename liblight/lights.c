1b1a0c7d145e9ea987fa8f8682f38e314c9b5019
guaiyihu <guaiyihu@foxmail.com>
2018-07-17 13:00:20 +0800

    gucci: liblights: Add button light entry
    Change-Id: I680182749c60dddf06a656a4e76241d4bc52d755

--- liblight/lights.c
+++ liblight/lights.c
@@ -60,6 +60,9 @@ const char *const GREEN_BLINK_FILE
 const char *const BLUE_BLINK_FILE
         = "/sys/class/leds/blue/blink";
 
+const char *const BUTTONS_FILE
+        = "/sys/class/leds/button-backlight/brightness";
+
 /**
  * device methods
  */
@@ -211,6 +214,22 @@ set_light_backlight(__attribute__ ((unused)) struct light_device_t *dev,
 }
 
 static int
+set_light_buttons(struct light_device_t *dev,
+        const struct light_state_t *state)
+{
+    int err = 0;
+    int brightness = rgb_to_brightness(state);
+
+    pthread_mutex_lock(&g_lock);
+
+    err = write_int(BUTTONS_FILE, brightness);
+
+    pthread_mutex_unlock(&g_lock);
+
+    return err;
+}
+
+static int
 set_light_attention(struct light_device_t *dev,
         const struct light_state_t *state)
 {
@@ -277,6 +296,8 @@ static int open_lights(const struct hw_module_t *module, const char *name,
 
     if (0 == strcmp(LIGHT_ID_BACKLIGHT, name))
         set_light = set_light_backlight;
+    else if (0 == strcmp(LIGHT_ID_BUTTONS, name))
+        set_light = set_light_buttons;
     else if (0 == strcmp(LIGHT_ID_NOTIFICATIONS, name))
         set_light = set_light_notifications;
     else if (0 == strcmp(LIGHT_ID_ATTENTION, name))
