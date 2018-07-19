de002283fd85c675f8bfac50fad71f243a9637ab
guaiyihu <guaiyihu@foxmail.com>
2018-07-19 23:32:45 +0800

    gucci: Import prebuilt wlan module from stock
    Change-Id: Ie0a8b7df8b580f76d8383e72bdc0e1d843966ab7

--- device.mk
+++ device.mk
@@ -241,3 +241,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
+    
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/pronto/pronto_wlan.ko:system/lib/modules/wlan.ko
