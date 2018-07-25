239739353c49c95341a582e8842d941da57a2f43
guaiyihu <guaiyihu@foxmail.com>
2018-07-25 11:50:08 +0800

    gucci: Import keylayouts from stock
    Change-Id: Ib08191493eaac88e666af0d6292881e1ea19fa9f

--- product/keylayouts.mk
+++ product/keylayouts.mk
@@ -1,4 +1,5 @@
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
-    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
+    $(LOCAL_PATH)/keylayout/Atmel_maXTouch_Touchscreen.kl:system/usr/keylayout/Atmel_maXTouch_Touchscreen.kl \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/ist30xx.kl:system/usr/keylayout/ist30xx.kl
