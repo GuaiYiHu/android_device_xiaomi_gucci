0889b6928d6929817c2ab2bd9c9cd6f361de9d92
guaiyihu <guaiyihu@foxmail.com>
2018-07-27 23:55:41 +0800

    gucci: Drop oss sensor hal
    Change-Id: I1994b1b0e8e041a8dda5bfa52d25bef2786511f4

--- device.mk
+++ device.mk
@@ -106,10 +106,7 @@ PRODUCT_COPY_FILES += \
 # Sensor HAL
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    calmodule.cfg \
-    libcalmodule_common \
-    sensors.msm8916 \
-    sensors.wt88047
+    sensors.msm8916
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/_hals.conf:system/vendor/etc/sensors/_hals.conf
