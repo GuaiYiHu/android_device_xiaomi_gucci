41a670d4edc0ff9b1d5885d084f5e609ad6b7b15
guaiyihu <guaiyihu@foxmail.com>
2018-07-28 02:19:51 +0800

    gucci: Update kernel flag
    Change-Id: I359c810627bb822fd32aacaedb06c781a8d4347f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,11 +53,14 @@ TARGET_LIBINIT_MSM8916_DEFINES_FILE := $(DEVICE_PATH)/init/init_gucci.cpp
 
 # Kernel
 BOARD_DTBTOOL_ARGS := -2
-BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_SEPARATED_DT := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/gucci
 TARGET_KERNEL_CONFIG := gucci_defconfig
 BOARD_KERNEL_CMDLINE += phy-msm-usb.floated_charger_enable=1
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_RAMDISK_OFFSET     := 0x01000000
 
 # Keymaster
 TARGET_PROVIDES_KEYMASTER := true
